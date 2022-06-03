a370e4498de7e0ad3d78212d072b4e26030af778
solowswan <solowswan@gmail.com>
2022-06-04 00:11:17 +0200

    4 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent  } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HeroesComponent,HeroDetailComponent,MessagesComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
