815a63274f66502b3da5231aeb52ab0eb94bd181
solowswan <solowswan@gmail.com>
2022-06-04 00:51:44 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from  '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatListModule} from '@angular/material/list'; 
+
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
@@ -14,7 +20,12 @@ import { MessagesComponent } from './messages/messages.component';
   imports:      [ BrowserModule, FormsModule, HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
-    )
+    ),
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatListModule
   ],
   declarations: [ AppComponent, HeroesComponent,HeroDetailComponent,MessagesComponent],
   bootstrap:    [ AppComponent ]
