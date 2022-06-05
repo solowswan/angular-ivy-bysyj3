25f718c6deaed82cf6140194f5bfcd7e8cf609d0
solowswan <solowswan@gmail.com>
2022-06-05 15:32:18 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,50 +6,49 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 
 interface FoodNode {
-  name: string;
+  "name": string;
   children?: FoodNode[];
 }
 
 const TREE_DATA: FoodNode[] = [
   {
-    name: 'Batches',
-    children: [
+    "name": "Batches",
+    "children": [
       {
-        name: 'Capture',children: [{name: 'Browse'},{name: 'New'}, ],
+        "name": "Capture","children": [{"name": "Browse"},{"name": "New"}]
       }, 
-      {name: 'Unification',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Grouping',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Transfer',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Enrichment',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Population',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Purging',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'Compression',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-      {name: 'DW Compression',children: [{name: 'Dashboard'},{name: 'Batches'}, {name: 'Schedule batches',children: [{name: 'New'}, ],}],      }, 
-    ],
-  },
-  
-  {
-    name: 'Staging Area',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+      {"name": "Unification","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "Grouping","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "Transfer","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"}]}]      }, 
+      {"name": "Enrichment","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "Population","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "Purging","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "Compression","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"} ]}]      }, 
+      {"name": "DW Compression","children": [{"name": "Dashboard"},{"name": "Batches"}, {"name": "Schedule batches","children": [{"name": "New"}]}]}
+    ]
   },
   {
-    name: 'UDS',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+    "name": "Staging Area",
+    "children": [{"name": "Apple"}, {"name": "Banana"}, {"name": "Fruit loops"}]
   },
   {
-    name: 'Configuration',
-    children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
+    "name": "UDS",
+    "children": [{"name": "Apple"}, {"name": "Banana"}, {"name": "Fruit loops"}]
   },
-  {name: 'Dashboard' }, 
-  {name: 'iDQM Portal' }, 
-  {name: 'Instruments' }, 
-  {name: 'Parties' }, 
-  {name: 'Codelists' }, 
-  {name: 'WS Parameters' }, 
-  {name: 'Worker jobs pool' }, 
-  {name: 'Incremental propagation' }, 
-  {name: 'System',children: [{name: 'Technical info'}, {name: 'DB pools',children: [{name: 'Main pool'}, {name: 'Caching pool'},{name: 'WH pool'}],},{name: 'Cache'}],      }, 
-
+  {
+    "name": "Configuration",
+    "children": [{"name": "Apple"}, {"name": "Banana"}, {"name": "Fruit loops"}]
+  },
+  {"name": "Dashboard" }, 
+  {"name": "iDQM Portal" }, 
+  {"name": "Instruments" }, 
+  {"name": "Parties" }, 
+  {"name": "Codelists" }, 
+  {"name": "WS Parameters" }, 
+  {"name": "Worker jobs pool" }, 
+  {"name": "Incremental propagation" }, 
+  {"name": "System","children": [{"name": "Technical info"}, {"name": "DB pools","children": [{"name": "Main pool"}, {"name": "Caching pool"},{"name": "WH pool"}]},{"name": "Cache"}]
+}
 ];
 
 
