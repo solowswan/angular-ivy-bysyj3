bf5eb8aef639789bfed01d136bc55eb46cb61ae1
solowswan <solowswan@gmail.com>
2022-06-07 00:30:19 +0200

    2 files modified

--- src/app/capturebatchbrowserdetails/capturebatchbrowserdetails.component.ts
+++ src/app/capturebatchbrowserdetails/capturebatchbrowserdetails.component.ts
@@ -1,4 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, ChangeDetectorRef } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {AppComponent} from '../app.component';
+import { MessageService } from '../message.service';
+import { Router } from '@angular/router';
+
+export interface BatchElement {
+  idcpbatch: number;
+  process_id: number;  
+  filename: string;
+  startdate: string;
+}
+
+const BATCH_DATA: BatchElement[] = [
+  {idcpbatch: 1, process_id:111, filename: 'SHSSCC.csv', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 1, process_id:111,filename: 'SHSSCC.xml', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 1, process_id:111,filename: 'DQMetric.json', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 2, process_id:111, filename: 'SHSSCC.csv', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 2, process_id:111,filename: 'SHSSCC.xml', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 2, process_id:111,filename: 'DQMetric.json', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 3, process_id:111, filename: 'SHSSCC.csv', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 3, process_id:111,filename: 'SHSSCC.xml', startdate:"2012-03-19T07:22Z"},
+  {idcpbatch: 3, process_id:111,filename: 'DQMetric.json', startdate:"2012-03-19T07:22Z"}
+];
 
 @Component({
   selector: 'app-capturebatchbrowserdetails',
@@ -6,8 +32,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./capturebatchbrowserdetails.component.css']
 })
 export class CapturebatchbrowserdetailsComponent implements OnInit {
+  displayedColumns: string[] = ['idcpbatch', 'process_id', 'filename','startdate'];
 
-  constructor() { }
+  constructor(public messageService: MessageService, 
+    private changeDetectorRefs: ChangeDetectorRef, 
+    private router: Router) {
+  }
+
+  dataSource = new MatTableDataSource(BATCH_DATA);
 
   ngOnInit() {
   }
